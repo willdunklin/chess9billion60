a56bb6861b137c9cc21b69e36eb0ce853bc5943c
Matthew Bolan <mjb6117@rit.edu>
2021-12-31 13:39:45 -0500

    More mild visualizer changes

--- src/visualizer.js
+++ src/visualizer.js
@@ -5,10 +5,25 @@ const PieceTypes = require("./pieces.js");
 
 export class Visualizer extends React.Component {
     render() {
+
+        const result_style = {
+            width: "240px",
+            height: "560px",
+            top: "0",
+            left: "0",
+
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: "1",
+            userSelect: "none",
+        }
+
         const container = {
             "position": "relative",
-            "width": "600px",
-            "height": "600px",
+            "width": "240px",
+            "height": "240px",
         };
 
         const board_style = {
@@ -16,21 +31,43 @@ export class Visualizer extends React.Component {
             "height": "85%",
         };
 
-        const x = this.props.square.toLowerCase().charCodeAt(0) - 97
-        const y = Number(this.props.square[1]) - 1
+        var x = 4
+        var y = 4
 
-        let dot_locations = PieceTypes[this.props.piece].getAvailableMoves(x,y,null,this.props.color).map(([to_x, to_y]) => `${String.fromCharCode(97 + (to_x))}${1+to_y}`) // map from coordinates to square
-        .map(to_square => `${this.props.color + this.props.piece}@${to_square}`); // of the form piece_name@to_square
-        let dots = [...new Set(dot_locations)]
+        let dot_locations = PieceTypes[this.props.piece].getAvailableMoves(x,y,null,"W").map(([to_x, to_y]) => `${String.fromCharCode(97 + (to_x))}${1+to_y}`) // map from coordinates to square
+        .map(to_square => `${"W" + this.props.piece}@${to_square}`); // of the form piece_name@to_square
+        let dots1 = [...new Set(dot_locations)]
+
+        x = 0
+        y = 0
+        dot_locations = PieceTypes[this.props.piece].getAvailableMoves(x,y,null,"W").map(([to_x, to_y]) => `${String.fromCharCode(97 + (to_x))}${1+to_y}`) // map from coordinates to square
+        .map(to_square => `${"W" + this.props.piece}@${to_square}`); // of the form piece_name@to_square
+        let dots2 = [...new Set(dot_locations)]
+
         return (
-            <div style={container}>
-                <div>
+            <div style={result_style}>
+                <div style={container}>
                     <div className="board" style={board_style}>
                         <Chess
-                            pieces={[this.props.color + this.props.piece + '@' + this.props.square]}
+                            pieces={["W" + this.props.piece + '@e5']}
                             highlights={[]}
                             drawLabels = {false}
-                            dots={dots}
+                            dots={dots1}
+                            onDragStart = {() => {return false;}}
+                            check={""}
+                            promotablePieces = {[]}
+                            whiteTurn={false}
+                            isWhite={true}
+                        />
+                    </div>
+                </div>
+                <div style={container}>
+                    <div className="board" style={board_style}>
+                        <Chess
+                            pieces={["W" + this.props.piece + '@a1']}
+                            highlights={[]}
+                            drawLabels = {false}
+                            dots={dots2}
                             onDragStart = {() => {return false;}}
                             check={""}
                             promotablePieces = {[]}
@@ -48,5 +85,4 @@ Visualizer.propTypes = {
     // vanilla react-chess
     color: PropTypes.string,
     piece: PropTypes.string,
-    square: PropTypes.string,
   }
