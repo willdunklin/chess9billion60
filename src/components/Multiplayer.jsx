a045893dfece4edbfd4a2dab9f2a94513a42ce96
willdunklin <dunkliw@clarkson.edu>
2022-01-05 19:30:57 -0500

    promising changes for heroku

--- src/components/Multiplayer.jsx
+++ src/components/Multiplayer.jsx
@@ -5,10 +5,12 @@ import { Chess } from "../Game";
 import { ChessBoard } from "../Board";
 import { useParams } from 'react-router-dom';
 
+
+const { protocol, hostname, port } = window.location;
 const ChessClient = Client({
     game: Chess,
     board: ChessBoard,
-    multiplayer: SocketIO({server: "localhost:8000"}),
+    multiplayer: SocketIO({server: `${protocol}//${hostname}:${port}`}),
     // debug: false,
     // multiplayer: SocketIO({server: "35.223.213.73:42069"})
 });
--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 const { Server, Origins } = require("boardgame.io/server");
 const { Chess } = require("./Game");
+const serve = require("koa-static");
+const path = require("path");
 
 const server = Server({
     games: [Chess],
     origins: [Origins.LOCALHOST]
 });
 
-server.run(8000);
+const PORT = process.env.PORT || 8000;
+
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+server.app.use(serve(frontEndAppBuildPath));
+
+server.run(PORT, () => {
+    server.app.use(
+        async (ctx, next) => serve(frontEndAppBuildPath)(
+            Object.assign(ctx, { path: 'index.html' }),
+            next
+        )
+    )
+});
