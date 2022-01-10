e5c1cdafe3468b8d40370a90e0991b07186982da
willdunklin <dunkliw@clarkson.edu>
2022-01-10 16:29:24 -0500

    warning fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Multiplayer } from './components/Multiplayer';
 import { Main } from './components/Main';
