5adf0eb6ae68e0110edabc8bc61fc94089e7e3c8
Matthew Bolan <mjb6117@rit.edu>
2022-01-17 10:42:41 -0500

    shitty example of how to do zoo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,35 @@ import { CookiesProvider, useCookies } from "react-cookie";
 import { nanoid } from "nanoid";
 import { New } from "./New";
 
+const { Visualizer } = require("./visualizer.js");
+const { PieceTypes } = require("../bgio/pieces");
+
+const visualizerStyles = {
+    paddingTop: "50px",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+}
+
+let visualizers = []
+let temp = []
+for (let [piece,value] of Object.entries(PieceTypes)) {
+    if (piece !== "P")
+        temp.push(piece)
+}
+temp.sort((a, b) => (PieceTypes[a].strength < PieceTypes[b].strength) ? -1 :1 )
+for (let piece of temp) {
+    visualizers.push(<Visualizer 
+        key={`${piece}-zoo-visualizer`} // fixes bug when promPieces changes
+        piece={piece} 
+        color={"W"}
+        count={5} // TODO this is a stupid name for this or a stupid way of doing this
+        />
+    )
+}
+
 const App = () => {
     const [ cookies, setCookie ] = useCookies(['user']);
     // Add cookie 
@@ -22,7 +51,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Nav />}>
                         <Route exact path="new" element={<New/>}/>
-                        <Route exact path="zoo" element={<h1>yo</h1>}/>
+                        <Route exact path="zoo" element={<div style={visualizerStyles}>{visualizers}</div>}/>
                         <Route exact path="patreon" element={<h1>patreon</h1>}/>
                         <Route exact path="settings" element={<h1>settings</h1>}/>
                         <Route exact path="" element={<Main />}/>
