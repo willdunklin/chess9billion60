f1aa6c358bd82820538962171aa69efb3c7bb0ce
Matthew Bolan <mjb6117@rit.edu>
2022-01-22 15:41:34 -0500

    fixed dot toggle bug highlighted selected piece

--- src/bgio/Board.js
+++ src/bgio/Board.js
@@ -79,6 +79,7 @@ export class ChessBoard extends React.Component {
             bTime: this.props.G.bTime,
             boardWidth: getSize(),
             historyIndex: 0,
+            lastClickedPiece: "",
         };
 
         this.onMovePiece = this.onMovePiece.bind(this);
@@ -103,6 +104,8 @@ export class ChessBoard extends React.Component {
         this.current_length = 0;
         this.curr_promotablePieces = [];
         this.gameover = false;
+
+        
         
         window.addEventListener('resize', this.handleResize)
 
@@ -221,16 +224,17 @@ export class ChessBoard extends React.Component {
 
     onClickPiece(piece, clear) {
         //either toggle the dots or run the clear
-        if(clear || this.state.dots.length > 0) {
+        if(clear || (this.state.dots.length > 0 && this.state.lastClickedPiece === piece.notation)) {
             // clear the dots on the screen
             this.setState({dots: []});
             return;
         }
+        this.setState({lastClickedPiece: piece.notation});
 
         const from_square = piece.position;
         const from_x = from_square.toLowerCase().charCodeAt(0) - 97;
         const from_y = Number(from_square[1]) - 1;
-
+        const clicked_square = piece.notation
         // get available moves, filter through validity check if its our turn
         if ((this.props.G.whiteTurn && piece.name.charAt(0) === "W") || (!this.props.G.whiteTurn && piece.name.charAt(0) !== "W") || this.state.historyIndex !== 0) {
             let dot_locations = PieceTypes[piece.name.substring(1)].getAvailableMoves(from_x, from_y, this.props.G.history.slice(this.state.historyIndex), piece.name.charAt(0))
@@ -238,7 +242,7 @@ export class ChessBoard extends React.Component {
                 .filter(to_square => // filter move validity (stolen from colorHasMateInOnes)
                     validMove(this.props.G.history.slice(this.state.historyIndex), piece.name, from_square, to_square) !== null)
                 .map(to_square => `${piece.name}@${to_square}`); // of the form piece_name@to_square
-
+            dot_locations.push(clicked_square)
             // set the new dot locations but remove duplicates
             this.setState({dots: [...new Set(dot_locations)]});
         } else {
@@ -246,7 +250,7 @@ export class ChessBoard extends React.Component {
             let dot_locations = PieceTypes[piece.name.substring(1)].getAvailableMoves(from_x, from_y, null, piece.name.charAt(0))
                 .map(([to_x, to_y]) => `${String.fromCharCode(97 + (to_x))}${1+to_y}`) // map from coordinates to square
                 .map(to_square => `${piece.name}@${to_square}`); // of the form piece_name@to_square
-
+            dot_locations.push(clicked_square)
             // set the new dot locations but remove duplicates
             this.setState({dots: [...new Set(dot_locations)]});
         }
@@ -371,7 +375,6 @@ export class ChessBoard extends React.Component {
     componentDidUpdate() {
         // this will run after every move
         if (this.current_length !== this.props.ctx.turn) {
-            console.log('hey');
             this.current_length = this.props.ctx.turn;
 
             // redraw board
