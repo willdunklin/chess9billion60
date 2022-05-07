50598205e951e3a771d9cfe8423ecb85eb9f6fac
willdunklin <will.dunklin@kitware.com>
2022-05-07 15:30:21 -0400

    revert to js static assets

--- src/bgio/sound.js
+++ src/bgio/sound.js
@@ -2,13 +2,16 @@ import { Howl } from 'howler';
 import Cookies from 'js-cookie';
 
 const Move = new Howl({
-    src: 'https://chess9b60.s3.amazonaws.com/sounds/Move.mp3'
+    // src: 'https://chess9b60.s3.amazonaws.com/sounds/Move.mp3'
+    src: require("../sounds/Move.ogg")
 });
 const Capture = new Howl({
-    src: 'https://chess9b60.s3.amazonaws.com/sounds/Capture.mp3'
+    // src: 'https://chess9b60.s3.amazonaws.com/sounds/Capture.mp3'
+    src: require("../sounds/Capture.ogg")
 });
 const GenericNotify = new Howl({
-    src: 'https://chess9b60.s3.amazonaws.com/sounds/GenericNotify.mp3'
+    // src: 'https://chess9b60.s3.amazonaws.com/sounds/GenericNotify.mp3'
+    src: require("../sounds/GenericNotify.ogg")
 });
 
 export const move = volume => {Move.volume(volume * (Cookies.get("volume") ?? 100) / 100); Move.play();}
