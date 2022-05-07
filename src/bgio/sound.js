6ccf8b11b1d86b2ac3629e0ea06572d751737b8d
willdunklin <will.dunklin@kitware.com>
2022-05-07 15:15:37 -0400

    Switch to use S3 for static content

--- src/bgio/sound.js
+++ src/bgio/sound.js
@@ -1,9 +1,16 @@
-import UIfx from 'uifx';
+import { Howl } from 'howler';
 import Cookies from 'js-cookie';
-const Move = new UIfx(require("../sounds/Move.ogg"));
-const Capture = new UIfx(require("../sounds/Capture.ogg"));
-const GenericNotify = new UIfx(require("../sounds/GenericNotify.ogg"));
 
-export const move = volume => Move.play((Cookies.get("volume") ?? 100) / 100 * volume);
-export const capture = volume => Capture.play((Cookies.get("volume") ?? 100) / 100 *volume);
-export const end = volume => GenericNotify.play((Cookies.get("volume") ?? 100) / 100 *volume);
+const Move = new Howl({
+    src: 'https://chess9b60.s3.amazonaws.com/sounds/Move.mp3'
+});
+const Capture = new Howl({
+    src: 'https://chess9b60.s3.amazonaws.com/sounds/Capture.mp3'
+});
+const GenericNotify = new Howl({
+    src: 'https://chess9b60.s3.amazonaws.com/sounds/GenericNotify.mp3'
+});
+
+export const move = volume => {Move.volume(volume * (Cookies.get("volume") ?? 100) / 100); Move.play();}
+export const capture = volume => {Capture.volume(volume * (Cookies.get("volume") ?? 100) / 100); Capture.play();}
+export const end = volume => {GenericNotify.volume(volume * (Cookies.get("volume") ?? 100) / 100); GenericNotify.play();}
--- src/react-chess/pieces/index.js
+++ src/react-chess/pieces/index.js
@@ -11,13 +11,8 @@ function make_piece(id) {
 }
 
 function Piece(props, id) {
-  let path = "./img/" + id.charAt(0) + "/" + id + ".png";
-  let myVar =  require(''+path)
-  if (typeof myVar === 'string' || myVar instanceof String) {
-    return React.createElement('img', { src: require(''+path), style: props, alt: '' + id, width: props.size, height: props.size });
-  } else {
-    return React.createElement('img', { src: require(''+path).default, style: props, alt: '' + id, width: props.size, height: props.size });
-  }
+  let url = "https://chess9b60.s3.amazonaws.com/img/" + id.charAt(0) + "/" + id + ".png";
+  return React.createElement('img', { src: url, style: props, alt: '' + id, width: props.size, height: props.size });
 }
 
 module.exports = function(id) {
