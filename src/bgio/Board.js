a3a1ac2098296a32a627f887a1fea2c3057299f4
willdunklin <dunkliw@clarkson.edu>
2022-08-06 15:39:17 -0400

    Add fix so spectators dont resign

--- src/bgio/Board.js
+++ src/bgio/Board.js
@@ -536,7 +536,7 @@ export class ChessBoard extends React.Component {
                                 </button>
                             </div>
                         </div>
-                        <div style={{display: 'flex'}}>
+                        <div style={{display: this.props.spectator ? 'none' : 'flex'}}>
                             <button style={{margin: 'auto', height: '2.5em'}} onClick={this.props.moves.resign}>
                                 <p style={{padding: '0 1em', margin: 'auto'}}>Resign</p>
                             </button>
