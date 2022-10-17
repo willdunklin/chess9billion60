be4a6003b5a66a5822771422c7c238ad0d63b3cb
willdunklin <will.dunklin@kitware.com>
2022-10-17 10:01:47 -0400

    Add betza changes

--- src/bgio/pieces.ts
+++ src/bgio/pieces.ts
@@ -1,11 +1,11 @@
-const W_leap = leaper(1, 0);
-const F_leap = leaper(1, 1);
-const N_leap = leaper(2, 1);
-const C_leap = leaper(3, 1);
-const Z_leap = leaper(3, 2);
-//const J_leap = leaper(4, 1);
-//const L_leap = leaper(4, 3);
-const K_leap = W_leap.concat(F_leap);
+const atomList : {[key : string] : number[][]} = {
+    "W" : leaper(1, 0),
+    "F" : leaper(1, 1),
+    "N" : leaper(2, 1),
+    "C" : leaper(3, 1),
+    "Z" : leaper(3, 2),
+    "K" : leaper(1, 0).concat(leaper(1, 1))
+}
 
 function leaper(dx: number, dy: number, x: number=0, y: number=0) {
     return [
@@ -25,19 +25,9 @@ function isInBounds(x: number, y: number) {
 }
 
 function rider(startx: number, starty: number, history: Array<Array<string|null>>|null, color: string, intervals: Array<Array<number>>, n = 7) {
+    //TODO there seems to be an implicit assumption here that the atoms never overlap eachother. Should probably prevent adding duplicate squares 
     let squares: Array<Array<number>> = [];
-    // let squares = intervals.map(
-    //     ([x, y]) => {
-    //         // look at continuation of vector to n steps
-    //         return [...Array(n-1).keys()].map(a => [(a+1)*x,(a+1)*y])
-    //     }).flat().map(
-    //         // adjust by offset
-    //         ([x, y]) => [startx+x, starty+y]
-    //     ).filter(
-    //         // filter for moves w/i bounds
-    //         ([x, y]) => isInBounds(x,y)
-    //     );
-    // squares = [...new Set(squares)];
+
     intervals.forEach(([dx, dy]: Array<number>) => {
         let x = startx + dx;
         let y = starty + dy;
@@ -66,7 +56,7 @@ function rider(startx: number, starty: number, history: Array<Array<string|null>
     return squares;
 }
 
-class Piece {
+export class Piece {
     id: string;
     strength: number;
     canPromote: boolean;
@@ -76,6 +66,7 @@ class Piece {
     blurb: string;
     ruleText: string;
     canMate: boolean;
+    betza: string;
 
     allowOnlyOne() {
         this.onlyOne = true;
@@ -97,7 +88,7 @@ class Piece {
         return this;
     }
 
-    getAvailableMoves = function (_x: number, _y: number, _gameboard: Array<Array<string|null>>|null, _color: string): Array<any> {
+    getAvailableMoves = function (_x: number, _y: number, _gameboard: Array<Array<string|null>>|null, _color: string): number[][] {
         return [];
     }
 
@@ -145,8 +136,25 @@ class Piece {
         return !this.canMate;
     }
 
-    constructor(id: string, strength: number, getAvailableMoves: (x: number, y: number, gameboard: Array<Array<string | null>> | null, color: string) => Array<any>) {
-        this.getAvailableMoves = getAvailableMoves;
+    constructor(id: string, strength: number, atoms: Array<[string, number]>, move_function?: (x: number, y: number, gameboard: Array<Array<string | null>> | null, color: string) => number[][]) {
+        if (move_function === undefined || move_function === null) {
+            this.getAvailableMoves = (x,y,gameboard,color) => {
+                let moves: number[][]
+                if (atoms[0][1] === 0)
+                        moves = rider(x,y,gameboard,color, atomList[atoms[0][0]])
+                    else
+                        moves = rider(x,y,gameboard,color, atomList[atoms[0][0]], atoms[0][1])
+                for (let i = 1; i < atoms.length; i++) {
+                    if (atoms[i][1] === 0)
+                        moves.push(...rider(x,y,gameboard,color, atomList[atoms[i][0]]))
+                    else
+                        moves.push(...rider(x,y,gameboard,color, atomList[atoms[i][0]], atoms[i][1]))
+                }
+                return moves
+            }
+        } else {
+            this.getAvailableMoves = move_function
+        }
         this.id = id;
         this.strength = strength;
         this.canPromote = false;
@@ -154,42 +162,50 @@ class Piece {
         this.onlyOne = false;
         this.pieceName = "Unnamed Piece";
         this.blurb = "Weird huh";
-        this.ruleText = "Moves like a knight";
+        this.ruleText = "Moves like Something";
         this.canMate = true;
+        this.betza = ""
+        if (id !== "P") {
+            for (let i = 0; i < atoms.length; i++) {
+                this.betza.concat(atoms[i][0] + atoms[i][1].toString())
+            }
+        } else {
+            this.betza = "fmWfceF"
+        }
     }
 }
 
 // TODO: export these variables (export const ...)
 // have to fix Game.js's use of PieceTypes then
-const N =   new Piece("N"  , 315, (x,y,gameboard,color) => {return rider(x,y,gameboard,color,N_leap,1)}).cantMate().name("Knight").setBlurb("A chess classic").setRules("Standard knight moves");
-const R =   new Piece("R"  , 500, (x,y,gameboard,color) => {return rider(x,y,gameboard,color,W_leap)}).name("Rook").setBlurb("A chess classic").setRules("Standard rook moves");
-const B =   new Piece("B"  , 315, (x,y,gameboard,color) => {return rider(x,y,gameboard,color,F_leap)}).markColorbound().name("Bishop").setBlurb("A chess classic").setRules("Standard bishop moves");
-const Q =   new Piece("Q"  , 975, (x,y,gameboard,color) => {return rider(x,y,gameboard,color,K_leap)}).allowOnlyOne().name("Queen").setBlurb("A chess classic").setRules("Standard queen moves");
-const K =   new Piece("K"  , 100000, (x,y,gameboard,color) => {return rider(x,y,gameboard,color,K_leap,1)}).allowOnlyOne().name("King").setBlurb("A chess classic").setRules("Standard king moves");
-const NR =  new Piece("NR" , 475 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,N_leap)}).cantMate().name("Knightrider").setBlurb("If knights weren't lazy").setRules("Knight moves, but can keep going");
-const M =   new Piece("M"  , 375 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,K_leap,1)}).name("Mann").setBlurb("Like the king, but poor").setRules("Moves like a King");
-const F =   new Piece("F"  , 150 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,F_leap,1)}).markColorbound().name("Ferz").setBlurb("Slow and steady wins the race").setRules("Bishop moves up to one square");
-const W =   new Piece("W"  , 170 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,W_leap,1)}).cantMate().name("Wazir").setBlurb("Wins some endgames").setRules("Rook moves up to one square");
-const A =   new Piece("A"  , 1250, (x,y,gameboard,color) => {return rider(x,y,gameboard,color,K_leap).concat(rider(x,y,gameboard,color,N_leap,1))}).allowOnlyOne().name("Amazon").setBlurb("Terrifying").setRules("Combo queen and knight");
-const CH =  new Piece("CH" , 800 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,W_leap).concat(rider(x,y,gameboard,color,N_leap,1))}).allowOnlyOne().name("Chancellor").setBlurb("The power behind the throne").setRules("Combo rook and knight");
-const AB =  new Piece("AB" , 770 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,F_leap).concat(rider(x,y,gameboard,color,N_leap,1))}).allowOnlyOne().name("Archbishop").setBlurb("Owns a cool cathedral").setRules("Combo bishop and knight");
-const R4 =  new Piece("R4" , 380 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,W_leap,4)}).name("Short Rook").setBlurb("Bullies the shorter rook").setRules("Rook moves up to four squares");
-const R2 =  new Piece("R2" , 270 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,W_leap,2)}).name("Shorter Rook").setBlurb("Bullies the wazir").setRules("Rook moves up to two squares");
-const B4 =  new Piece("B4" , 250 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,F_leap,4)}).markColorbound().name("Short Bishop").setBlurb("Not as good as a bishop").setRules("Bishop moves up to four squares");
-const B2 =  new Piece("B2" , 220 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,F_leap,2)}).markColorbound().name("Shorter Bishop").setBlurb("Not as good as a short bishop").setRules("Bishop moves up to two squares");
-const U =   new Piece("U"  , 900 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,F_leap).concat(rider(x,y,gameboard,color,N_leap))}).allowOnlyOne().name("Unicorn").setBlurb("Twelve directions!").setRules("Combo knightrider and bishop");
-const C =   new Piece("C"  , 220 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,C_leap,1)}).markColorbound().name("Camel").setBlurb("Cannot leave its color").setRules("3 one way 1 the other");
-const Z =   new Piece("Z"  , 180 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,Z_leap,1)}).cantMate().name("Zebra").setBlurb("Annoying to maneuver").setRules("3 one way 2 the other");
-const ZC =  new Piece("ZC" , 400 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,Z_leap,1).concat(rider(x,y,gameboard,color,C_leap,1))}).allowOnlyOne().name("Zebramel").setBlurb("Watch out for smothered mates!").setRules("Combo Camel (3,1) and Zebra (3,2)");
-const CN =  new Piece("CN" , 600 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,K_leap,1).concat(rider(x,y,gameboard,color,N_leap,1))}).name("Centaur").setBlurb("A strong piece with limited range").setRules("King and Knight moves");
-const CNR = new Piece("CNR", 900 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,K_leap,1).concat(rider(x,y,gameboard,color,N_leap))}).allowOnlyOne().name("Centaur Rider").setBlurb("Scary").setRules("King and Knightrider moves");
-const BC =  new Piece("BC" , 750 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,C_leap,1).concat(rider(x,y,gameboard,color,F_leap))}).markColorbound().name("Bishop Camel").setBlurb("Queen tier, but just on one color!").setRules("Combo Camel (3,1) and Bishop");
-const NZ =  new Piece("NZ" , 600 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,N_leap,1).concat(rider(x,y,gameboard,color,Z_leap,1))}).cantMate().allowOnlyOne().name("Zorse").setBlurb("Do not leave holes in your position.").setRules("Combo Zebra (3,2) and Knight");
-const M2 =  new Piece("M2" , 500 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,K_leap,2)}).name("Freddie Mercury").setBlurb("Part of Queen!").setRules("Queen moves up to two squares");
-//made up strength values
-const BM =   new Piece("BM" , 550 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,K_leap,1).concat(rider(x,y,gameboard,color,F_leap))}).name("Cardinal").setBlurb("Can reach both colors!").setRules("Combo Bishop and King");
-const RM =   new Piece("RM" , 700 , (x,y,gameboard,color) => {return rider(x,y,gameboard,color,K_leap,1).concat(rider(x,y,gameboard,color,W_leap))}).name("Rook Mann").setBlurb("The rook's been working out").setRules("Combo Rook and King");
-const P =  new Piece("P", 100, (x, y, history, color) => {
+const N   =  new Piece("N"  , 315   , [["N", 1]]).name("Knight").setBlurb("A chess classic").setRules("Standard knight moves").cantMate();
+const R   =  new Piece("R"  , 500   , [["W", 0]]).name("Rook").setBlurb("A chess classic").setRules("Standard rook moves");
+const B   =  new Piece("B"  , 315   , [["F", 0]]).name("Bishop").setBlurb("A chess classic").setRules("Standard bishop moves").markColorbound();
+const Q   =  new Piece("Q"  , 975   , [["K", 0]]).name("Queen").setBlurb("A chess classic").setRules("Standard queen moves").allowOnlyOne();
+const K   =  new Piece("K"  , 100000, [["K", 1]]).name("King").setBlurb("A chess classic").setRules("Standard king moves").allowOnlyOne();
+const NR  =  new Piece("NR" , 475   , [["N", 0]]).name("Knightrider").setBlurb("If knights weren't lazy").setRules("Knight moves, but can keep going").cantMate();
+const M   =  new Piece("M"  , 375   , [["K", 1]]).name("Mann").setBlurb("Like the king, but poor").setRules("Moves like a King");
+const F   =  new Piece("F"  , 150   , [["F", 1]]).name("Ferz").setBlurb("Slow and steady wins the race").setRules("Bishop moves up to one square").markColorbound();
+const W   =  new Piece("W"  , 170   , [["W", 1]]).name("Wazir").setBlurb("Wins some endgames").setRules("Rook moves up to one square").cantMate();
+const A   =  new Piece("A"  , 1250  , [["K", 0], ["N", 1]]).name("Amazon").setBlurb("Terrifying").setRules("Combo queen and knight").allowOnlyOne();
+const CH  =  new Piece("CH" , 800   , [["W", 0], ["N", 1]]).name("Chancellor").setBlurb("The power behind the throne").setRules("Combo rook and knight").allowOnlyOne();
+const AB  =  new Piece("AB" , 770   , [["F", 0], ["N", 1]]).name("Archbishop").setBlurb("Owns a cool cathedral").setRules("Combo bishop and knight").allowOnlyOne();
+const R4  =  new Piece("R4" , 380   , [["W", 4]]).name("Short Rook").setBlurb("Bullies the shorter rook").setRules("Rook moves up to four squares");
+const R2  =  new Piece("R2" , 270   , [["W", 2]]).name("Shorter Rook").setBlurb("Bullies the wazir").setRules("Rook moves up to two squares");
+const B4  =  new Piece("B4" , 250   , [["F", 4]]).name("Short Bishop").setBlurb("Not as good as a bishop").setRules("Bishop moves up to four squares").markColorbound();
+const B2  =  new Piece("B2" , 220   , [["F", 2]]).name("Shorter Bishop").setBlurb("Not as good as a short bishop").setRules("Bishop moves up to two squares").markColorbound();
+const U   =  new Piece("U"  , 900   , [["N", 0], ["F", 0]]).name("Unicorn").setBlurb("Twelve directions!").setRules("Combo knightrider and bishop").allowOnlyOne();
+const C   =  new Piece("C"  , 220   , [["C", 1]]).name("Camel").setBlurb("Cannot leave its color").setRules("3 one way 1 the other").markColorbound();
+const Z   =  new Piece("Z"  , 180   , [["Z", 1]]).name("Zebra").setBlurb("Annoying to maneuver").setRules("3 one way 2 the other").cantMate();
+const ZC  =  new Piece("ZC" , 400   , [["Z", 1], ["C", 1]]).name("Zebramel").setBlurb("Watch out for smothered mates!").setRules("Combo Camel (3,1) and Zebra (3,2)").allowOnlyOne();
+const CN  =  new Piece("CN" , 600   , [["N", 1], ["K", 1]]).name("Centaur").setBlurb("A strong piece with limited range").setRules("King and Knight moves");
+const CNR =  new Piece("CNR", 900   , [["N", 0], ["K", 1]]).name("Centaur Rider").setBlurb("Scary").setRules("King and Knightrider moves").allowOnlyOne();
+const BC  =  new Piece("BC" , 750   , [["F", 0], ["C", 1]]).name("Bishop Camel").setBlurb("Queen tier, but just on one color!").setRules("Combo Camel (3,1) and Bishop").markColorbound();
+const NZ  =  new Piece("NZ" , 600   , [["N", 1], ["Z", 1]]).name("Zorse").setBlurb("Do not leave holes in your position.").setRules("Combo Zebra (3,2) and Knight").cantMate().allowOnlyOne();
+const M2  =  new Piece("M2" , 500   , [["K", 2]]).name("Freddie Mercury").setBlurb("Part of Queen!").setRules("Queen moves up to two squares");
+const BM  =  new Piece("BM" , 550   , [["K", 1], ["F", 0]]).name("Cardinal").setBlurb("Can reach both colors!").setRules("Combo Bishop and King");
+const RM  =  new Piece("RM" , 700   , [["K", 1], ["W", 0]]).name("Rook Mann").setBlurb("The rook's been working out").setRules("Combo Rook and King");
+//Pawn is very special
+const P =  new Piece("P", 100, [], (x, y, history, color) => {
         let direction = 1;
         let home_rank = 1;
         let en_passant_rank = 4;
@@ -260,9 +276,9 @@ const P =  new Piece("P", 100, (x, y, history, color) => {
             }
         }
         return moves;
-    }).makePromoter().name("Pawn").setBlurb("Google en passant").setRules("Standard pawn moves");
+    }).name("Pawn").setBlurb("Google en passant").setRules("Standard pawn moves").makePromoter();
 
-export const PieceTypes = {
+export const PieceTypes: { [key: string]: Piece } = {
     "N": N,
     "R": R,
     "B": B,
