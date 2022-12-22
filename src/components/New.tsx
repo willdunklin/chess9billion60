c14f375a6d8792231849fa526de990a38c199ada
willdunklin <dunkliw@clarkson.edu>
2022-12-22 09:35:09 -0500

    Update /play menu

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useCallback } from "react";
 import axios from 'axios';
 import { Navigate, useNavigate } from 'react-router-dom';
-import Modal from 'react-modal';
 import { useCookies } from "react-cookie";
 import { MultiRangeSlider } from "./MultiRangeSlider";
 
@@ -16,6 +15,7 @@ const New = () => {
     const [ time, setTime ] = useState(900);
     const [ increment, setIncrement ] = useState(10);
     const [ enableTimer, setEnableTimer ] = useState(true);
+    const [ url, setUrl ] = useState("");
     const [ gameid, setGameid ] = useState("");
     const [ cookies ] = useCookies(['idtoken']);
 
@@ -58,12 +58,10 @@ const New = () => {
         })
         .then(res => {
             if (res.status === 200) {
-                // console.log('gameid: ', res.data);
                 setGameid(res.data);
                 setLoadedSuccessfully(true);
                 setIsOpen(false);
             }
-            // console.log(res);
         })
         .catch(e => {
             console.log('error!', e);
@@ -84,7 +82,6 @@ const New = () => {
                     return false;
                 if (gid === 'timeout') {
                     setLobby(-1);
-                    // refresh_players();
                     return false;
                 }
 
@@ -114,10 +111,24 @@ const New = () => {
         navigate(-1);
     }
 
+    const join_url = () => {
+        if (url === "")
+            return;
+
+        const url_split = url.split('/');
+        const gameid = url_split[url_split.length - 1];
+
+        if (!gameid || gameid.length !== 6)
+            return;
+
+        setGameid(gameid);
+        setLoadedSuccessfully(true);
+        setIsOpen(false);
+    }
+
     React.useEffect(() => {
         const interval = setInterval(() => {
             join_pool(lobby);
-            // refresh_players();
         }, 4500);
 
         if (lobby === -1)
@@ -130,88 +141,107 @@ const New = () => {
     if (loadedSuccessfully)
         return <Navigate to={`/${gameid}`}/>;
 
-    Modal.setAppElement("#root");
-
     return (
-        <div>
-            <Modal isOpen={isOpen}>
-                <aside>
-                    <h2 style={{marginBottom: 0, marginTop: '1em'}}>Join a Lobby</h2>
-                    <p><i>Choose a time control</i></p>
-                </aside>
-                <div>
-                    <button onClick={() => { join_lobby(0) }} className={lobby === 0 ? "buttonHighlight" : ""}>
-                        3 | 2
-                    </button>
-                    <button onClick={() => { join_lobby(1) }} className={lobby === 1 ? "buttonHighlight" : ""}>
-                        5 | 3
-                    </button>
-                    <button onClick={() => { join_lobby(2) }} className={lobby === 2 ? "buttonHighlight" : ""}>
-                        10 | 10
-                    </button>
-                    {/* <button onClick={refresh_players}>Refresh</button> */}
+        <>
+            <div className="games">
+                <div className="gameCol" id="joinGame">
+                    <div className="gameCol" style={{width: '100%'}}>
+                        <aside>
+                            <h2 style={{marginBottom: 0, marginTop: '1em'}}>Join A Game</h2>
+                            <p className="modalAside" style={{margin: '0.5em 0 0 0'}}><i>Enter a game id/url</i></p>
+                        </aside>
+                        <div className="gameRow" style={{alignItems: 'baseline'}}>
+                            <p>Game ID:</p>
+                            <input type="text"
+                                defaultValue={url}
+                                onChange={event => {
+                                    setUrl( event.target.value );
+                                }}
+                            />
+                            <div className='link' style={{padding: '0.9em 0.5em'}} onClick={join_url}>Join</div>
+                        </div>
+                    </div>
                 </div>
-                <aside>
-                    <h2 style={{marginBottom: 0, marginTop: '1.2em'}}>Play With a Friend</h2>
-                    <p><i>(Share the link once customized game is created)</i></p>
-                </aside>
-                <div>
-                    <p>Color: </p>
-                    <>
-                        <button onClick={() => {setIsWhite("0")}} className={isWhite === "0" ? "buttonHighlight" : ""}>White</button>
-                        <button onClick={() => {setIsWhite("1")}} className={isWhite === "1" ? "buttonHighlight" : ""}>Black</button>
-                        <button onClick={() => {setIsWhite("random")}} className={isWhite === "random" ? "buttonHighlight" : ""}>Random</button>
-                    </>
+
+                <div className="gameCol" id="customGame">
+                    <aside>
+                        <h2 style={{marginBottom: 0, marginTop: '1.2em'}}>Create a Custom Game</h2>
+                        <p className="modalAside"><i>(Share the link once customized game is created)</i></p>
+                    </aside>
+                    <div className="gameRow">
+                        <p>Color: </p>
+                        <button onClick={() => {setIsWhite("0")}} className={isWhite === "0" ? "buttonHighlight specButton" : "specButton"}>White</button>
+                        <button onClick={() => {setIsWhite("1")}} className={isWhite === "1" ? "buttonHighlight specButton" : "specButton"}>Black</button>
+                        <button onClick={() => {setIsWhite("random")}} className={isWhite === "random" ? "buttonHighlight specButton" : "specButton"}>Random</button>
+                    </div>
+                    <div className="gameRow">
+                        <p>Enable Timers:</p>
+                        <input type="checkbox"
+                            id="darkMode"
+                            name="darkMode"
+                            checked={enableTimer}
+                            onChange={event => {
+                                    setEnableTimer(event.target.checked);
+                            }}
+                        />
+                    </div>
+                    <div className="gameRow">
+                        <p>Start time: </p>
+                        <input type="text"
+                            defaultValue={time}
+                            onChange={event => {
+                                setTime( Number(event.target.value) );
+                            }}
+                        />
+                    </div>
+                    <div className="gameRow">
+                        <p>Increment: </p>
+                        <input type="text"
+                            defaultValue={increment}
+                            onChange={event => {
+                                setIncrement( Number(event.target.value) );
+                            }}
+                        />
+                    </div>
+                    <div className="gameRow">
+                        <p>Strength:</p>
+                        <MultiRangeSlider
+                            min={20}
+                            max={55}
+                            startLeft={30}
+                            startRight={40}
+                            onChange={(min: number, max: number) => {
+                                // Set min/max between 20- and 55+
+                                setMinStrength(min === 20 ? 0 : min);
+                                setMaxStrength(max === 55 ? 1000 : max);
+                            }}
+                        />
+                    </div>
+                    <div className="links gameRow">
+                        <div className='link' onClick={close}>Cancel</div>
+                        <div className='link' onClick={start_game}>Start</div>
+                    </div>
                 </div>
-                <div>
-                    <p>Enable Timers:</p>
-                    <input type="checkbox"
-                           id="darkMode"
-                           name="darkMode"
-                           checked={enableTimer}
-                           onChange={event => {
-                                setEnableTimer(event.target.checked);
-                           }}
-                    />
+
+                <div className="gameCol">
+                    <aside>
+                        <h2 style={{marginBottom: 0, marginTop: '1em'}}>Join a Lobby</h2>
+                        <p className="modalAside"><i>Choose a time control</i></p>
+                    </aside>
+                    <div className="gameRow">
+                        <button onClick={() => { join_lobby(0) }} className={lobby === 0 ? "buttonHighlight specButton" : "specButton"}>
+                            3 | 2
+                        </button>
+                        <button onClick={() => { join_lobby(1) }} className={lobby === 1 ? "buttonHighlight specButton" : "specButton"}>
+                            5 | 3
+                        </button>
+                        <button onClick={() => { join_lobby(2) }} className={lobby === 2 ? "buttonHighlight specButton" : "specButton"}>
+                            10 | 10
+                        </button>
+                    </div>
                 </div>
-                <div>
-                    <p>Start time: </p>
-                    <input type="text"
-                           defaultValue={time}
-                           onChange={event => {
-                               setTime( Number(event.target.value) );
-                           }}
-                    />
-                </div>
-                <div>
-                    <p>Increment: </p>
-                    <input type="text"
-                           defaultValue={increment}
-                           onChange={event => {
-                               setIncrement( Number(event.target.value) );
-                           }}
-                    />
-                </div>
-                <div>
-                    <p>Strength:</p>
-                    <MultiRangeSlider
-                        min={20}
-                        max={55}
-                        startLeft={30}
-                        startRight={40}
-                        onChange={(min: number, max: number) => {
-                            // Set min/max between 20- and 55+
-                            setMinStrength(min === 20 ? 0 : min);
-                            setMaxStrength(max === 55 ? 1000 : max);
-                        }}
-                    />
-                </div>
-                <div className="links">
-                    <div className='link' onClick={close}>Cancel</div>
-                    <div className='link' onClick={start_game}>Start</div>
-                </div>
-            </Modal>
-        </div>
+            </div>
+        </>
     );
 }
 
